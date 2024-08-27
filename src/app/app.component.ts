854e809556c8682e96e91d496fc90972127bb017
ilDug <dognini.marco@gmail.com>
2024-08-27 15:24:34 +0200

    initial scaffolding

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'bj-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styles: [],
+})
+export class AppComponent {
+  title = 'blackjack';
+}
